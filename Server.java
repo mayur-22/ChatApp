6b51390c5f91c1817c1e6d41347a6ed3a02fcc11
kamalashraf1 <46621032+kamalashraf1@users.noreply.github.com>
2019-09-08 00:58:41 +0530

    Add files via upload

--- Server.java
+++ Server.java
@@ -2,67 +2,116 @@ import java.io.*;
 import java.net.*;
 import java.lang.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.security.*;
 
-class Server throws Exception{
-
-	private ArrayList<Thread> table;
-
-	public static void main(String[] args) {
-		ServerSocket welcomeSocket = new ServerSocket(2200);
-		table = new ArrayList<Thread>();
-
-		while(true){
-			Socket connSocket = welcomeSocket.accept();
-
-			//new client is connected
-			System.out.println("New Client: "+connSocket);
-
-			
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
-			String recMsg[] = inFromClient.readLine().split(" ");
-
-			if(recMsg[1].equals("TOSEND")){
-				Thread sen = new ServerSender(connSocket);
-				
-				/***check by parsing***/
-				while(true){
-					boolean wellFormed = true;
-					//if username is well formed
-					if(wellFormed){
-						boolean exists = false;
-						//check if name already exists
-
-						if(exists){
-							String retMsg = "ERROR 104 Already Registered\n\n";
-							DataOutputStream outToClient = new DataOutputStream(connSocket.getOutputStream());
-							outToClient.writeBytes(retMsg);
-						}
-						else{
-							String retMsg = "Registered TOSEND [username]\n\n";
-							DataOutputStream outToClient = new DataOutputStream(connSocket.getOutputStream());
-							outToClient.writeBytes(retMsg);
-							break;
-						}
-					}
-					else{
-						String retMsg = "ERROR 100 Malformed username\n\n";
-						DataOutputStream outToClient = new DataOutputStream(connSocket.getOutputStream());
-						outToClient.writeBytes(retMsg);
-					}
-
-					recMsg[] = inFromClient.readLine().split(" ");
-				}
-				sen.start();
-			}
-
-			else if(recMsg[1].equals("TOREC")){
-				Thread rec = new ServerReceiver(connSocket);
-				//it will be received correctly
-				rec.start();
-				table.add(rec);
-				rec.close();
-			}
-
+class Server
+{
+    ConcurrentHashMap<String,Socket> ReceivingTable;
+    ConcurrentHashMap<String,PublicKey> KeyTable;
+    ArrayList<String> Registered;
+    class ServerSender implements Runnable
+    {
+        boolean success;
+        Socket sender;
+        BufferedReader inFromClient ;
+        DataOutputStream outToClient;
+        
+        public ServerSender(Socket incoming, String[] recMsg) {
+            success = false;
+            sender = incoming;
+            try
+            {
+            inFromClient = new BufferedReader(new InputStreamReader(sender.getInputStream()));
+            outToClient = new DataOutputStream(sender.getOutputStream());
+            
+            boolean wellFormed = true;
+	    //if username is well formed
+	    if(wellFormed)
+            {
+	        boolean exists = false;
+	        if(Registered.contains(recMsg[1]))
+                    exists = false;
+                else
+                    exists = checkName(recMsg[1]);
+                
+        	if(exists)
+                {
+		    String retMsg = "ERROR 104 Already Registered\n\n";
+                    outToClient.writeBytes(retMsg);
+                }
+		else
+                {
+		    String retMsg = "Registered TOSEND [username]\n\n";
+		    outToClient.writeBytes(retMsg);
 		}
-	}
-}
+	    }
+	    else
+            {
+	        String retMsg = "ERROR 100 Malformed username\n\n";
+		outToClient.writeBytes(retMsg);
+	    }
+           
+            }catch(IOException e)
+            {
+                System.out.println("Error communicating with socket "+ incoming.getInetAddress() + " "+incoming.getPort());
+            }
+        }
+        
+        boolean checkName(String s)
+        {
+            return true;
+        }
+        @Override
+        public void run()
+        {
+
+        }
+        
+    }
+    
+    class ServerReceiver implements Runnable
+    {
+        boolean success;
+        public ServerReceiver(Socket incoming, String[] recMsg) {
+            success = false;
+        }
+        @Override
+        public void run()
+        {
+
+        }
+        
+    }
+    void runServer(int mode)throws Exception
+    {
+        
+        ServerSocket welcomeSocket = new ServerSocket(2200);
+        while(true)
+        {
+            Socket incoming = welcomeSocket.accept();
+            System.out.println("\nNew Scoket Create" + incoming.getPort());
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+	    String recMsg[] = inFromClient.readLine().split(" ");
+            if(recMsg[1].equals("TOSEND"))
+            {
+                ServerSender newSender = new ServerSender(incoming,recMsg);
+                if(newSender.success)
+                {
+                    Thread t = new Thread(newSender);
+                    t.start();
+                    Registered.add(recMsg[1]);
+                }
+            }
+            else if(recMsg[1].equals("TORCV"))
+            {
+                ServerReceiver  newReceiver = new ServerReceiver(incoming,recMsg);
+                if(newReceiver.success)
+                {
+                    ReceivingTable.put(recMsg[1],incoming);
+                }
+            }
+        }
+        
+    }
+}	
