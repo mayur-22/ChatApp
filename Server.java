37f0dc8d71c3a9cd1fa84e9d2f3c43f6c2671780
kamalashraf1 <46621032+kamalashraf1@users.noreply.github.com>
2019-09-09 20:59:02 +0530

    Update Server.java

--- Server.java
+++ Server.java
@@ -8,6 +8,7 @@ import java.security.*;
 class Server
 {
     ConcurrentHashMap<String,Socket> ReceivingTable;
+    ConcurrentHashMap<String,Socket> SendingTable;
     ConcurrentHashMap<String,PublicKey> KeyTable;
     ArrayList<String> Registered;
     class ServerSender implements Runnable
@@ -16,8 +17,14 @@ class Server
         Socket sender;
         BufferedReader inFromClient ;
         DataOutputStream outToClient;
+        String username;
         
-        public ServerSender(Socket incoming, String[] recMsg) {
+        public String getName()
+        {
+         return username;   
+        }                
+        public ServerSender(Socket incoming, String[] recMsg) 
+        {
             success = false;
             sender = incoming;
             try
@@ -25,26 +32,19 @@ class Server
             inFromClient = new BufferedReader(new InputStreamReader(sender.getInputStream()));
             outToClient = new DataOutputStream(sender.getOutputStream());
             
-            boolean wellFormed = true;
-	    //if username is well formed
+            boolean wellFormed = false;
+	    wellFormed = checkName(recMsg[1]);
 	    if(wellFormed)
             {
-	        boolean exists = false;
-	        if(Registered.contains(recMsg[1]))
-                    exists = false;
-                else
-                    exists = checkName(recMsg[1]);
-                
-        	if(exists)
+	        if(Registered.contains(username))
                 {
-		    String retMsg = "ERROR 104 Already Registered\n\n";
+                    String retMsg = "ERROR 104 Already Registered\n\n";
                     outToClient.writeBytes(retMsg);
                 }
-		else
-                {
-		    String retMsg = "Registered TOSEND [username]\n\n";
-		    outToClient.writeBytes(retMsg);
-		}
+        	
+	        String retMsg = "REGISTERED TOSEND ["+username+"]\n\n";
+	        outToClient.writeBytes(retMsg);
+	
 	    }
 	    else
             {
@@ -55,17 +55,112 @@ class Server
             }catch(IOException e)
             {
                 System.out.println("Error communicating with socket "+ incoming.getInetAddress() + " "+incoming.getPort());
+                System.out.println(Arrays.toString(e.getStackTrace()));
             }
         }
         
-        boolean checkName(String s)
+        private boolean checkName(String s)
         {
+            if(!s.endsWith("]") || !s.startsWith("["))
+                return false;
+            s = s.substring(1,s.length()-1);
+            char[] array = s.toCharArray();
+            for(char c:array)
+            {
+                if((c>='a' && c<='z')||(c>='A' && c<='Z') || (c>='0' && c<='9')) 
+                    continue;
+                return false;
+            }
+            this.username = s;
             return true;
         }
         @Override
         public void run()
         {
-
+          while(true)
+          {
+              try
+              {
+                  String Message = inFromClient.readLine();
+                  int mLength = checkHeader(Message);
+                  if(mLength>=0)
+                  {
+                      String[] splitted = Message.split("\n");
+                      if(splitted.length>=4)
+                      {
+                          if((splitted[3].startsWith("[")) && splitted[3].endsWith("]"))
+                          {
+                              int a = splitted[3].indexOf('[');
+                              int b = splitted[3].lastIndexOf(']');
+                              if(b-a-1 == mLength)
+                              {
+                                  String destName = splitted[1].substring(7,splitted[1].length()-1);
+                                  String res = "FORWARD ["+ username + "]\n" + splitted[2] + "\n\n";
+                                  res = res + "["  + splitted[3].substring(a+1,b)+"]";
+                                  
+                                  if(Registered.contains(destName))
+                                  {                                      
+                                  Socket s = ReceivingTable.get(destName);
+                                  ServerReceiver recv = new ServerReceiver(s,destName,username,res);
+                                  Thread t = new Thread(recv);
+                                  t.start();
+                                  }
+                              }
+                              else
+                              {
+                                  outToClient.writeBytes("ERROR 102 Unable to send\\n\n");
+                              }
+                          }
+                      }
+                      else
+                          outToClient.writeBytes("ERROR 102 Unable to send\\n\n");
+                  }
+                  else
+                  {
+                      outToClient.writeBytes("ERROR 103 Header incomplete\\n\n");
+                  }
+                  
+              
+              }
+              catch(IOException e)
+              {
+                  System.out.println("Error receiving message from the port "+ sender.getPort());
+              }
+          }
+        }
+        
+        private int checkHeader(String s)
+        {
+            String[] splitLines = s.split("\n",2);
+            if (splitLines.length >=1)
+            {
+                String[] firstline = splitLines[0].split("\\[");
+                if(firstline[0].equals("SEND "))
+                {
+                    String names[] = firstline[1].split("']'");
+                    if(!Registered.contains((names[0])))
+                        return -1;
+                    
+                }
+                    
+                if(splitLines[1].startsWith("Content-Length: [") && splitLines[1].endsWith("]"))
+                {
+                    int a = splitLines[1].indexOf('[');
+                    int b = splitLines[1].indexOf(']');
+                    if(b==splitLines[1].length())
+                    {
+                        String lng = splitLines[1].substring(a+1,b);
+                        int length = Integer.parseInt(lng);
+                        return length;
+                    }
+                    else
+                        return -1;
+                }
+                    
+            }
+            else
+             return -1;
+            return -1;
         }
         
     }
@@ -73,14 +168,73 @@ class Server
     class ServerReceiver implements Runnable
     {
         boolean success;
-        public ServerReceiver(Socket incoming, String[] recMsg) {
+        Socket socket;
+        String message;
+        String username;
+        String sendname;
+        BufferedReader inStream;
+        DataOutputStream outStream; 
+        public ServerReceiver(Socket incoming, String recMsg) 
+        {
             success = false;
+            if(recMsg.charAt(0)=='[' && recMsg.charAt(recMsg.length()-1)==']')
+            {
+              if(Registered.contains(recMsg.substring(1, recMsg.length()-1)))
+              {
+                  socket = incoming;
+                  success = true;
+                  username = recMsg.substring(1,recMsg.length()-1);
+                  try
+                  {
+                  inStream = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+                  outStream = new DataOutputStream(incoming.getOutputStream());
+                  }
+                  catch(IOException e)
+                  {
+                      success = false;
+                  }
+              }
+              
+            }   
+  
+        }
+        public ServerReceiver(Socket incoming,String thisname, String othername,String messgae)
+        {
+            success = true;
+            socket = incoming;
+            username = thisname;
+            sendname = othername;
+            try
+            {
+                inStream = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+                outStream = new DataOutputStream(incoming.getOutputStream());
+            }
+            catch(IOException e)
+            {
+                success = false;
+            }
         }
         @Override
         public void run()
         {
-
+          try
+          {
+           outStream.writeBytes(message);
+           String msg = inStream.readLine();
+           if(msg.equals("RECEIVED ["+username+"]\n"))
+           {
+               Socket s = SendingTable.get(sendname);
+               DataOutputStream senderOut = new DataOutputStream(s.getOutputStream());
+               senderOut.writeBytes("SENT ["+sendname+"]\n");
+           }
+          }
+          catch(IOException e)
+          {
+              throw new RuntimeException(e);
+          }
+          
         }
+        
         
     }
     void runServer(int mode)throws Exception
@@ -90,7 +244,7 @@ class Server
         while(true)
         {
             Socket incoming = welcomeSocket.accept();
-            System.out.println("\nNew Scoket Create" + incoming.getPort());
+            System.out.println("\nNew Scoket Created" + incoming.getPort());
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
 	    String recMsg[] = inFromClient.readLine().split(" ");
             if(recMsg[1].equals("TOSEND"))
@@ -98,17 +252,19 @@ class Server
                 ServerSender newSender = new ServerSender(incoming,recMsg);
                 if(newSender.success)
                 {
+                    
                     Thread t = new Thread(newSender);
                     t.start();
-                    Registered.add(recMsg[1]);
+                    Registered.add(newSender.username);
+                    SendingTable.put(newSender.username,newSender.sender);
                 }
             }
-            else if(recMsg[1].equals("TORCV"))
+            else if(recMsg[1].equals("TORECV"))
             {
-                ServerReceiver  newReceiver = new ServerReceiver(incoming,recMsg);
+                ServerReceiver  newReceiver = new ServerReceiver(incoming,recMsg[1]);
                 if(newReceiver.success)
                 {
-                    ReceivingTable.put(recMsg[1],incoming);
+                    ReceivingTable.put(recMsg[1].substring(1,recMsg[1].length()-1),incoming);
                 }
             }
         }
